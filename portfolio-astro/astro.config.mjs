2a85b9332727c4fe12592cb305e2b553529e7a39
Skeegan123 <mcfeegan123@gmail.com>
2026-03-03 23:22:43 -0800

    Revert "Add Cloudflare Workers configuration"
    This reverts commit 2f11c4fef52b9a135699b721f61f489975e7e71c.

--- portfolio-astro/astro.config.mjs
+++ portfolio-astro/astro.config.mjs
@@ -2,12 +2,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://keegangaffney.com',
   output: 'static',
-  integrations: [sitemap()],
-  adapter: cloudflare()
+  integrations: [sitemap()]
 });
